90740dad319662a67707183c50d507f5252d0194
Kylie B <114032967+KayBirdy@users.noreply.github.com>
2024-03-01 14:17:11 -0500

    NavBar

---
+++ src/NavBar.js
@@ -0,0 +1,10 @@
+function NavBar() {
+  return (
+    <div className="NavBar">
+    <a href="#title">NavBar</a>
+      <a class="active" href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#contact">Contact</a>
+    </div>
+  )
+}
