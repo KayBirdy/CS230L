fcd810b1a87ae36bf74b7990ef755e21722c6016
Kylie B <114032967+KayBirdy@users.noreply.github.com>
2024-03-03 12:20:37 -0500

    Added justify content between NavBar 	modified:   src/NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,7 +3,7 @@ import './NavBar.css';
 
 function NavBar() {
 return(
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -12,7 +12,7 @@ return(
     <div class="collapse navbar-collapse">
       <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="#">Home <span class="sr-only"></span></a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">About</a>
