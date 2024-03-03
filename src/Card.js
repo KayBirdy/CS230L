bc93d234f95e6d433ef18bdd13dde2daaf215f7d
Kylie B <114032967+KayBirdy@users.noreply.github.com>
2024-03-03 11:48:05 -0500

    Added card.js 	new file:   src/Card.js

---
+++ src/Card.js
@@ -0,0 +1,33 @@
+import App from './App';
+
+function Card() {
+return(
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="card text-white bg-primary">
+        <div class="card-body">
+          <h5 class="card-title">Card 01</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="ard text-white bg-danger">
+        <div class="card-body">
+          <h5 class="card-title">Card 02</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="ard text-white bg-success">
+        <div class="card-body">
+          <h5 class="card-title">Card 03</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+}
+export default Card;
